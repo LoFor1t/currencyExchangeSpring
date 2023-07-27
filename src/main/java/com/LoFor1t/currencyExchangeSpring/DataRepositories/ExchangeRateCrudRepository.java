e045e2324fdb9aa0d6a13ef70173210a515cdb88
LoFor1t <vgresnyh@gmail.com>
2023-07-27 20:01:34 +0200

    Create get request for getting exchange rate by currency codes.

--- src/main/java/com/LoFor1t/currencyExchangeSpring/DataRepositories/ExchangeRateCrudRepository.java
+++ src/main/java/com/LoFor1t/currencyExchangeSpring/DataRepositories/ExchangeRateCrudRepository.java
@@ -1,7 +1,11 @@
 package com.LoFor1t.currencyExchangeSpring.DataRepositories;
 
+import com.LoFor1t.currencyExchangeSpring.DataModels.Currency;
 import com.LoFor1t.currencyExchangeSpring.DataModels.ExchangeRate;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface ExchangeRateCrudRepository extends CrudRepository<ExchangeRate, Integer> {
+    Optional<ExchangeRate> findByBaseCurrencyAndTargetCurrency(Optional<Currency> baseCurrency, Optional<Currency> targetCurrency);
 }
---
+++ src/main/java/com/LoFor1t/currencyExchangeSpring/RestControllers/ExchangeRateController.java
@@ -0,0 +1,48 @@
+package com.LoFor1t.currencyExchangeSpring.RestControllers;
+
+import com.LoFor1t.currencyExchangeSpring.DataModels.Currency;
+import com.LoFor1t.currencyExchangeSpring.DataModels.ExchangeRate;
+import com.LoFor1t.currencyExchangeSpring.DataRepositories.CurrencyCrudRepository;
+import com.LoFor1t.currencyExchangeSpring.DataRepositories.ExchangeRateCrudRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/exchangeRate")
+public class ExchangeRateController {
+    private final ExchangeRateCrudRepository exchangeRateCrudRepository;
+    private final CurrencyCrudRepository currencyCrudRepository;
+
+    public ExchangeRateController(ExchangeRateCrudRepository exchangeRateCrudRepository, CurrencyCrudRepository currencyCrudRepository) {
+        this.exchangeRateCrudRepository = exchangeRateCrudRepository;
+        this.currencyCrudRepository = currencyCrudRepository;
+    }
+
+    @GetMapping("/{codes}")
+    public ResponseEntity<Object> getExchangeRateByCurrencyCodes(@PathVariable String codes) {
+        if (codes.length() != 6) {
+            return new ResponseEntity<>("Код валютной пары отсутствует в адресе", HttpStatus.BAD_REQUEST);
+        }
+
+        Optional<Currency> baseCurrency = currencyCrudRepository.findByCode(codes.substring(0, 3));
+        Optional<Currency> targetCurrency = currencyCrudRepository.findByCode(codes.substring(3, 6));
+
+        if (baseCurrency.isEmpty() || targetCurrency.isEmpty()) {
+            return new ResponseEntity<>("Одной из валют нет в базе данных.", HttpStatus.NOT_FOUND);
+        }
+
+        Optional<ExchangeRate> exchangeRate = exchangeRateCrudRepository.findByBaseCurrencyAndTargetCurrency(baseCurrency, targetCurrency);
+
+        if (exchangeRate.isEmpty()) {
+            return new ResponseEntity<>("Обменный курс для пары не найден.", HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(exchangeRate, HttpStatus.OK);
+    }
+}

52976f9d8fd384de473d65107868e7a413f7f9c1
LoFor1t <vgresnyh@gmail.com>
2023-07-27 15:35:56 +0200

    Initial commit

---
+++ src/main/java/com/LoFor1t/currencyExchangeSpring/CurrencyExchangeSpringApplication.java
@@ -0,0 +1,13 @@
+package com.LoFor1t.currencyExchangeSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyExchangeSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyExchangeSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LoFor1t/currencyExchangeSpring/CurrencyExchangeSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LoFor1t.currencyExchangeSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
